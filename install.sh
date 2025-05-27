073c8cb398da0ddf639b5ee2dc27c061619ed373
Soumendu Bagchi <soumendubagchi@gmail.com>
2025-05-27 15:33:24 -0400

    updated lammps package-list in the general ubuntu install.

--- install.sh
+++ install.sh
@@ -9,9 +9,14 @@ git clone https://github.com/lammps/lammps.git
 cd lammps
 mkdir build
 cd build
-cmake ../cmake -D BUILD_MPI=yes -D PKG_MANYBODY=yes -D PKG_MOLECULE=yes
+cmake  -D CMAKE_BUILD_TYPE=Release \
+            -D LAMMPS_EXCEPTIONS=ON \
+            -D BUILD_SHARED_LIBS=ON \
+            -D PKG_MANYBODY=ON -D PKG_MC=ON -D PKG_MOLECULE=ON -D PKG_KSPACE=ON -D PKG_REPLICA=ON -D PKG_ASPHERE=ON -D PKG_ML-SNAP=ON -D PKG_REAXFF=ON \
+            -D PKG_RIGID=ON -D PKG_MPIIO=ON -D PKG_QEQ=ON -D PKG_PYTHON=On \
+            -D PKG_INTERLAYER=ON -D CMAKE_POSITION_INDEPENDENT_CODE=ON -D CMAKE_EXE_FLAGS="-dynamic" ../cmake
 make -j4
-make install
+make install-python
 
 # Install Python dependencies
 pip install -r requirements.txt
