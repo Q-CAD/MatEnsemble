796590949deb1578a40bb0092892db13e4e09a56
Soumendu Bagchi <soumendubagchi@gmail.com>
2025-05-27 20:24:37 -0400

    modify ovito install

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Create and activate conda environment
-conda env create -f environment.yaml
-conda init
-conda activate matensemble
+conda env create -f environment.yaml --prefix $1
+conda activate $1
 
 # Clone and build LAMMPS
-#git clone https://github.com/lammps/lammps.git
+git clone https://github.com/lammps/lammps.git
 cd lammps
 mkdir build
 cd build
@@ -16,11 +15,11 @@ cmake  -D CMAKE_BUILD_TYPE=Release \
             -D PKG_MANYBODY=ON -D PKG_MC=ON -D PKG_MOLECULE=ON -D PKG_KSPACE=ON -D PKG_REPLICA=ON -D PKG_ASPHERE=ON -D PKG_ML-SNAP=ON -D PKG_REAXFF=ON \
             -D PKG_RIGID=ON -D PKG_MPIIO=ON -D PKG_QEQ=ON -D PKG_PYTHON=On \
             -D PKG_INTERLAYER=ON -D CMAKE_POSITION_INDEPENDENT_CODE=ON -D CMAKE_EXE_FLAGS="-dynamic" ../cmake
-make -j4
+make -j64
 make install-python
 
-# Install Ovito
-conda install --strict-channel-priority -c https://conda.ovito.org -c conda-forge ovito=3.12.1
+# Install Ovito (with -y flag to automatically accept prompts)
+conda install -y --strict-channel-priority -c https://conda.ovito.org -c conda-forge ovito=3.12.1
 
 # Install Python dependencies
 pip install -r requirements.txt
